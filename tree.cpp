2b3fb759ce6804c3108eb545335add44439f416a
iqbalgn72 <iqbalgn72@gmail.com>
2024-06-13 15:56:27 +0700

    preorder

--- tree.cpp
+++ tree.cpp
@@ -79,9 +79,22 @@ public:
         {
             inorder(ptr->leftchild);
             cout << ptr->info <<" ";
-            inorder(ptr->rightchild);
-        
-            
+            inorder(ptr->rightchild);          
+        }
+    }
+
+    void preorder(node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            cout << ptr->info <<" ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);          
         }
     }
 };
