34f680c5dc3f96c661f8dbd2edcf7fe5f40e50dc
iqbalgn72 <iqbalgn72@gmail.com>
2024-06-13 16:08:48 +0700

    case 1

--- tree.cpp
+++ tree.cpp
@@ -129,5 +129,17 @@ int main()
         char ch;
         cin >> ch;
         cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word ;
+            x.insert (word);
+            break;
+        }
+        }
     }
 }
